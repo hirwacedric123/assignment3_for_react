3e38df519ba3f1ee34bf2b2cbd6f0d7530180561
hirwa cedric <hirwacedr12@gmail.com>
2024-12-05 09:29:09 +0200

    finishing adding required on fields

--- my-react-app/src/components/LecturerRegistrationForm.jsx
+++ my-react-app/src/components/LecturerRegistrationForm.jsx
@@ -31,10 +31,10 @@ function LecturerRegistrationForm() {
         <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
             <h3>Lecturer Registration</h3>
             <input name="name" required placeholder="Name" value={formData.name} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
-            <input name="email" placeholder="Email" value={formData.email} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
+            <input required name="email" placeholder="Email" value={formData.email} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
             {errors.email && <p style={{color:'red'}}>{errors.email}</p>}
-            <input name="subject" placeholder="Subject" value={formData.subject} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
-            <input name="phone" placeholder="Phone Number" value={formData.phone} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
+            <input required name="subject" placeholder="Subject" value={formData.subject} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
+            <input required name="phone" placeholder="Phone Number" value={formData.phone} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
             {errors.phone && <p style={{color:'red'}}>{errors.phone}</p>}
             <button 
                     type="submit" 
--- my-react-app/src/components/StudentRegistrationForm.jsx
+++ my-react-app/src/components/StudentRegistrationForm.jsx
@@ -28,12 +28,12 @@ function StudentRegistrationForm() {
         <div className="component-card">
             <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
             <h3>Student Registration</h3>
-            <input name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
+            <input required name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
             <input name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange} required style={{ padding: '10px', borderRadius: '5px' }} />
             <input name="email" placeholder="Email" value={formData.email} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
-            <input name="studentId" placeholder="Student ID" value={formData.studentId} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
+            <input required name="studentId" placeholder="Student ID" value={formData.studentId} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
             {errors.studentId && <p>{errors.studentId}</p>}
-            <input name="dob" type="date" placeholder="Date of Birth" value={formData.dob} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
+            <input required name="dob" type="date" placeholder="Date of Birth" value={formData.dob} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
             <button 
                     type="submit" 
                     style={{ padding: '10px', backgroundColor: '#3498db', color: '#fff', border: 'none', borderRadius: '5px' }}
