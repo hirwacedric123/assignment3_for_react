89c9070b6b6f2d342686fd2d4d7cd46cb8c11597
hirwa cedric <hirwacedr12@gmail.com>
2024-12-11 20:56:11 +0200

    changing student registration forms

--- my-react-app/src/components/27.StudentRegistrationForm.jsx
+++ my-react-app/src/components/27.StudentRegistrationForm.jsx
@@ -28,12 +28,20 @@ function StudentRegistrationForm() {
         <div className="component-card">
             <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
             <h2>Student Registration</h2>
-            <input required name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
-            <input name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange} required style={{ padding: '10px', borderRadius: '5px' }} />
-            <input name="email" placeholder="Email" value={formData.email} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
-            <input required name="studentId" placeholder="Student ID" value={formData.studentId} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
+            <input type="text"  required name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
+            <input type="text"  name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange} required style={{ padding: '10px', borderRadius: '5px' }} />
+            <input type="text"  name="email" placeholder="Email" value={formData.email} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
+            <input type="text"  required name="studentId" placeholder="Student ID" value={formData.studentId} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
             {errors.studentId && <p>{errors.studentId}</p>}
-            <input required name="dob" type="date" placeholder="Date of Birth" value={formData.dob} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
+            <input
+                required
+                name="dob"
+                type="date"
+                placeholder="Date of Birth"
+                value={formData.dob}
+                onChange={handleChange}
+            />
+
             <button 
                     type="submit" 
                     style={{ padding: '10px', backgroundColor: '#3498db', color: '#fff', border: 'none', borderRadius: '5px' }}
