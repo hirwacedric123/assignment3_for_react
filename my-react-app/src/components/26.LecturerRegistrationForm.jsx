83f4cdfc435c620318074dbde2369e8370fb3348
hirwa cedric <hirwacedr12@gmail.com>
2024-12-11 20:51:40 +0200

    changing lecturer registration form style

--- my-react-app/src/components/26.LecturerRegistrationForm.jsx
+++ my-react-app/src/components/26.LecturerRegistrationForm.jsx
@@ -30,11 +30,11 @@ function LecturerRegistrationForm() {
             
         <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
             <h2>Lecturer Registration</h2>
-            <input name="name" required placeholder="Name" value={formData.name} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
-            <input required name="email" placeholder="Email" value={formData.email} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
+            <input type="text"  name="name" required placeholder="Name" value={formData.name} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
+            <input type="text"  required name="email" placeholder="Email" value={formData.email} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }}/>
             {errors.email && <p style={{color:'red'}}>{errors.email}</p>}
-            <input required name="subject" placeholder="Subject" value={formData.subject} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
-            <input required name="phone" placeholder="Phone Number" value={formData.phone} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
+            <input type="text"  required name="subject" placeholder="Subject" value={formData.subject} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
+            <input type="text"  required name="phone" placeholder="Phone Number" value={formData.phone} onChange={handleChange} style={{ padding: '10px', borderRadius: '5px' }} />
             {errors.phone && <p style={{color:'red'}}>{errors.phone}</p>}
             <button 
                     type="submit" 
